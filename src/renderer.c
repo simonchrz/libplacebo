simonchrz/libplacebo

commit 0758cbe03ace108c73628cdd07095fa26a465c63

Revert "renderer: never use linear downscaling on HDR sources"
This reverts commit 030b59d1771cdfe7c563480aa187dbc11645ca36.

Since we have antiringing support for EWA scalers now, we have no need to
not use linear scaling for HDR content.

Closes: https://github.com/haasn/libplacebo/issues/281

Signed-off-by: Shengyu Qu <wiagn@4d2.org>

src/renderer.c
@@ -2031,10 +2031,12 @@ static bool pass_scale_main(struct pass_state *pass)
     if (params->disable_linear_scaling || fbofmt->component_depth[0] < 16)
         use_sigmoid = use_linear = false;
 
-    // Avoid sigmoidization for HDR content because it clips to [0,1], and
-    // linearization because it causes very nasty ringing artefacts.
-    if (pl_color_space_is_hdr(&img->color))
-        use_sigmoid = use_linear = false;
+    // Avoid sigmoidization for HDR content because it clips to [0,1]
+    if (pl_color_space_is_hdr(&img->color)) {
+        use_sigmoid = false;
+        if (fbofmt->type != PL_FMT_FLOAT)
+            use_linear = false; // linear HDR needs out-of-range signals
+    }
 
     if (!(use_linear || use_sigmoid) && img->color.transfer == PL_COLOR_TRC_LINEAR) {
         img->color.transfer = image->color.transfer;

commit 1398e4815c967fc0e5e44f47cde0998a5c846791

renderer: move max_peak detection out of hdr_update_peak()
Enabling linear downscaling has an accidental side effect to break HDR peak
detection mechanism, fix it by running the max peak value detect logic before
linearize.

With commit 0758cbe0, pl_color_transfer_nominal_peak() inside
hdr_update_peak() would return 1.0 when linear downscaling is applied to HDR
content instead of PQ/HLG's actual maximum nominal peak. Do the max peak
detection before linear downscaling so that the actual value could be
preserved.

Closes: https://github.com/haasn/libplacebo/issues/371

Fixes: 0758cbe0 ("Revert "renderer: never use linear downscaling on HDR sources"")
Signed-off-by: Shengyu Qu <wiagn@4d2.org>
(cherry picked from commit 8f5ec6128c66f859b699592fdc0a8056d637ccee)

src/renderer.c
@@ -1180,7 +1180,7 @@ hook_error:
     return ret;
 }
 
-static void hdr_update_peak(struct pass_state *pass)
+static void hdr_update_peak(struct pass_state *pass, float max_peak)
 {
     const struct pl_render_params *params = pass->params;
     pl_renderer rr = pass->rr;
@@ -1196,8 +1196,6 @@ static void hdr_update_peak(struct pass_state *pass)
     if (!rr->gpu->limits.max_ssbo_size)
         goto cleanup;
 
-    float max_peak = pl_color_transfer_nominal_peak(pass->img.color.transfer) *
-                     PL_COLOR_SDR_WHITE;
     if (pass->img.color.transfer == PL_COLOR_TRC_HLG)
         max_peak = pass->img.color.hdr.max_luma;
     if (max_peak <= pass->target.color.hdr.max_luma + 1e-6)
@@ -1993,10 +1991,12 @@ static bool pass_scale_main(struct pass_state *pass)
     struct sampler_info info = sample_src_info(pass, &src, SAMPLER_MAIN);
     bool use_sigmoid = info.dir == SAMPLER_UP && params->sigmoid_params;
     bool use_linear  = info.dir == SAMPLER_DOWN;
+    float max_peak = pl_color_transfer_nominal_peak(img->color.transfer) *
+                     PL_COLOR_SDR_WHITE;
 
     // Opportunistically update peak here if it would save performance
     if (info.dir == SAMPLER_UP)
-        hdr_update_peak(pass);
+        hdr_update_peak(pass, max_peak);
 
     // We need to enable the full rendering pipeline if there are any user
     // shaders / hooks that might depend on it.
@@ -2078,7 +2078,7 @@ static bool pass_scale_main(struct pass_state *pass)
 
 done:
     if (info.dir != SAMPLER_UP)
-        hdr_update_peak(pass);
+        hdr_update_peak(pass, max_peak);
     pass_hook(pass, img, PL_HOOK_SCALED);
     return true;
 }